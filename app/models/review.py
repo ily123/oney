9aed82964690b087bfc89f6717b9be58c2320ecc
suchitra <suchimohan91@gmail.com>
2021-12-06 13:51:36 -0800

    review model and seeders done

--- app/models/review.py
+++ app/models/review.py
@@ -7,7 +7,7 @@ class Review(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=True)
     product_id = db.Column(db.Integer, db.ForeignKey("products.id"), nullable=True)
-    content = b.Column(db.Text, nullable=False)
+    content = db.Column(db.Text, nullable=False)
     rating = db.Column(db.Integer, nullable=False)
     created_at = db.Column(db.DateTime(timezone=True), server_default=func.now())
     updated_at = db.Column(db.DateTime(timezone=True), server_default=func.now())
