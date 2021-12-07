258cb8cbd2d25f599201911fc113756debfcd6d9
suchitra <suchimohan91@gmail.com>
2021-12-07 13:13:12 -0800

    added products card

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -3,12 +3,13 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import LoginForm from './components/auth/LoginForm';
 import SignUpForm from './components/auth/SignUpForm';
-import NavBar from './components/NavBar';
+import Navigation from './components/Navigation';
 import ProtectedRoute from './components/auth/ProtectedRoute';
 import UsersList from './components/UsersList';
 import User from './components/User';
 import { authenticate } from './store/session';
 import SingleProductPage from './components/SingleProductPage';
+import Top20Products from './components/Top20Products';
 
 function App() {
   const [loaded, setLoaded] = useState(false);
@@ -27,7 +28,7 @@ function App() {
 
   return (
     <BrowserRouter>
-      <NavBar />
+      <Navigation />
       <Switch>
         <Route path='/login' exact={true}>
           <LoginForm />
@@ -44,9 +45,10 @@ function App() {
         <Route path='/products/:productId' exact={true}>
           <SingleProductPage />
         </Route>
-        <ProtectedRoute path='/' exact={true} >
-          <h1>My Home Page</h1>
-        </ProtectedRoute>
+        <Route path='/' exact={true} >
+          
+          <Top20Products />
+        </Route>
       </Switch>
     </BrowserRouter>
   );
