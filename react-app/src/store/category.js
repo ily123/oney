f38783853ce91a54f4addb955b36a6f26feb3713
ily123 <ibnovikov@gmail.com>
2021-12-07 12:57:26 -0600

    Added thunk and action to category store.

--- react-app/src/store/category.js
+++ react-app/src/store/category.js
@@ -1,4 +1,21 @@
-const SET_TREE = "dummy"
+const SET_TREE = "category/SET_TREE";
+
+const setTree = (tree) => {
+  return {
+    type: SET_TREE,
+    tree
+  }
+}
+
+export const fetchCategoryTree = () => async (dispatch) => {
+  const response = await fetch('api/category/');
+  if (response.ok) {
+    const tree = await response.json()
+    if (tree.errors) return
+    console.log(tree)
+    dispatch(setTree(tree))
+  }
+}
 
 const initialState = {
   tree: null
