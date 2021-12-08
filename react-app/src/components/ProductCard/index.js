253a07be6d47c0b972ace557197a930533bade3e
suchitra <suchimohan91@gmail.com>
2021-12-08 08:59:31 -0800

    cards nav not working

--- react-app/src/components/ProductCard/index.js
+++ react-app/src/components/ProductCard/index.js
@@ -1,17 +1,19 @@
 import './ProductCard.css'
-// import { NavLink } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 const ProductCard = ({id,price,images}) => {
 
-    console.log(images)
+    console.log(id)
     let image = images[0]?.url_570xN
 
     if (image) {
         return (
-                <div className='product_card'>
-                <img className= 'productImage' src={image} alt=""/>
-                <span className='productPrice'>${price}</span>
-                </div>
+                <NavLink to={`/products/${id}`}>
+                    <div className='product_card'>
+                        <img className= 'productImage' src={image} alt=""/>
+                        <span className='productPrice'>${price}</span>
+                    </div>
+                </NavLink>
             );
         }
     return null
