e1744623a09afb92ce8bf3a6e4a57c4d60e6a316
ily123 <ibnovikov@gmail.com>
2021-12-08 11:31:32 -0600

    Added imports to category page.

--- react-app/src/components/CategoryPage/index.js
+++ react-app/src/components/CategoryPage/index.js
@@ -1,6 +1,14 @@
-import {useParams } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
+import ProductCard from '../ProductCard';
+import { useDispatch, useSelector }from 'react-redux';
+import { fetechCategoryTree } from '../../store/category';
 
 export default function CategoryPage() {
   const { categoryId } = useParams();
-  return <h2>{`Category ${categoryId} items will go here.`}</h2>
+  
+  return (
+    <div>
+      <h2>{`Category ${categoryId} items will go here.`}</h2>
+    </div>
+  )
 }
