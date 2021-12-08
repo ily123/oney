d253537a0882c12d421c720e478b37f389912d6f
suchitra <suchimohan91@gmail.com>
2021-12-08 14:25:54 -0800

    styled forms

--- react-app/src/components/AddProductForm/index.js
+++ react-app/src/components/AddProductForm/index.js
@@ -1,15 +1,20 @@
 import {useState} from "react";
-import {useDispatch} from "react-redux"
+import { useDispatch, useSelector } from 'react-redux';
 import {useHistory} from "react-router";
 import {addOneProduct} from "../../store/product";
 import './AddProductForm.css'
 
 const AddProductForm = () =>{
+
+    const categoryTree = useSelector(state=>state.category)
+    console.log(categoryTree)
+
     const [title,setTitle] = useState('');
     const [description,setDescription] = useState('')
     const [price, setPrice] = useState('')
     const [category,setCategory] = useState('68887312')
     const [image,setImage] = useState('')
+
 
 
     const dispatch = useDispatch();
@@ -37,48 +42,64 @@ const AddProductForm = () =>{
 
     return (
         <div className='add-Product-Div'>
-            <h2>List your product details</h2>
+            <h2>Let's get started! Tell us about your product</h2>
             <form onSubmit={handleSubmit} className='add-product'>
-                <input
-                onChange={(e)=>setTitle(e.target.value)}
-                value={title}
-                placeholder='Enter Product Title'
-                required
-                />
-                <textarea
-                onChange={(e)=>setDescription(e.target.value)}
-                value={description}
-                placeholder='Enter Product Description'
-                required
-                />
+                <div>
+                    <label>Product Title</label>
+                    <input
+                    onChange={(e)=>setTitle(e.target.value)}
+                    value={title}
+                    placeholder='Enter Product Title'
+                    required
+                    />
+                </div>
+                <div>
+                    <label>Product Description</label>
+                    <textarea
+                    onChange={(e)=>setDescription(e.target.value)}
+                    value={description}
+                    placeholder='Enter Product Description'
+                    required
+                    />
+                </div>
+                <div>
                 <label> Category </label>
                 <select onChange={(e)=>setCategory(e.target.value)}>
                     <option value= "68887312" >Fine Art</option>
                     <option value= "68887366" >Handmade Holiday Items</option>
                     <option value="68887482">Handmade jewelry</option>
                 </select>
-                <input
-                onChange={(e)=>setPrice(e.target.value)}
-                value={price}
-                placeholder= "Price Per Product"
-                required
-                type="number"
-                min = "1"
-                max = "1000"
-                />
-                <input
-                onChange={(e)=>setImage(e.target.value)}
-                value={image}
-                placeholder= "Product Image URL"
-                required
-                type="url"
-                />
-                <button className='submit-button' type='submit'>
-                    Submit
-                </button>
-                <button className='submit-button' type='submit' onClick={()=>{handleCancel()}}>
-                    Cancel
-                </button>
+                </div>
+                <div>
+                    <label>Product Price</label>
+                    <input
+                    onChange={(e)=>setPrice(e.target.value)}
+                    value={price}
+                    placeholder= "Price Per Product"
+                    required
+                    type="number"
+                    min = "1"
+                    max = "1000"
+                    />
+                </div>
+                <div>
+                    <label>Product Image</label>
+                    <input
+                    onChange={(e)=>setImage(e.target.value)}
+                    value={image}
+                    placeholder= "Product Image URL"
+                    required
+                    type="url"
+                    />
+                </div>
+                <div className="button_div">
+                    <button className='submit-button' type='submit'>
+                        Submit
+                    </button>
+                    <button className='submit-button' type='submit' onClick={()=>{handleCancel()}}>
+                        Cancel
+                    </button>
+                </div>
             </form>
         </div>
     )
--- react-app/src/components/auth/SignUpForm.js
+++ react-app/src/components/auth/SignUpForm.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux'
 import { Redirect } from 'react-router-dom';
 import { signUp } from '../../store/session';
+import { useHistory } from "react-router";
 
 const SignUpForm = () => {
   const [errors, setErrors] = useState([]);
@@ -11,6 +12,7 @@ const SignUpForm = () => {
   const [repeatPassword, setRepeatPassword] = useState('');
   const user = useSelector(state => state.session.user);
   const dispatch = useDispatch();
+  const history = useHistory();
 
   const onSignUp = async (e) => {
     e.preventDefault();
@@ -22,6 +24,9 @@ const SignUpForm = () => {
     }
   };
 
+  const handleCancel = () => {
+    history.push('/')
+  }
   const updateUsername = (e) => {
     setUsername(e.target.value);
   };
@@ -43,51 +48,58 @@ const SignUpForm = () => {
   }
 
   return (
-    <form onSubmit={onSignUp}>
-      <div>
-        {errors.map((error, ind) => (
-          <div key={ind}>{error}</div>
-        ))}
-      </div>
-      <div>
-        <label>User Name</label>
-        <input
-          type='text'
-          name='username'
-          onChange={updateUsername}
-          value={username}
-        ></input>
-      </div>
-      <div>
-        <label>Email</label>
-        <input
-          type='text'
-          name='email'
-          onChange={updateEmail}
-          value={email}
-        ></input>
-      </div>
-      <div>
-        <label>Password</label>
-        <input
-          type='password'
-          name='password'
-          onChange={updatePassword}
-          value={password}
-        ></input>
-      </div>
-      <div>
-        <label>Repeat Password</label>
-        <input
-          type='password'
-          name='repeat_password'
-          onChange={updateRepeatPassword}
-          value={repeatPassword}
-          required={true}
-        ></input>
-      </div>
-      <button type='submit'>Sign Up</button>
-    </form>
+    <div className = "form-Div">
+      <form onSubmit={onSignUp} className="form_placing">
+        <div className="errors_div">
+          {errors.map((error, ind) => (
+            <div key={ind}>{error}</div>
+          ))}
+        </div>
+        <div>
+          <label>User Name</label>
+          <input
+            type='text'
+            name='username'
+            onChange={updateUsername}
+            value={username}
+          ></input>
+        </div>
+        <div>
+          <label>Email</label>
+          <input
+            type='text'
+            name='email'
+            onChange={updateEmail}
+            value={email}
+          ></input>
+        </div>
+        <div>
+          <label>Password</label>
+          <input
+            type='password'
+            name='password'
+            onChange={updatePassword}
+            value={password}
+          ></input>
+        </div>
+        <div>
+          <label>Repeat Password</label>
+          <input
+            type='password'
+            name='repeat_password'
+            onChange={updateRepeatPassword}
+            value={repeatPassword}
+            required={true}
+          ></input>
+        </div>
+        <div className="button_div">
+        <button className='submit-button' type='submit'>Sign Up</button>
+        <button className='submit-button' type='submit' onClick={()=>{handleCancel()}}>
+              Cancel
+        </button>
+        </div>
+      </form>
+    </div>
   );
 };
 
