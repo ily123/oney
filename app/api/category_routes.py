1f3d02fbc2a0445d85d47c4e89ba1a159bcbe9a5
ily123 <ibnovikov@gmail.com>
2021-12-09 18:20:03 -0600

    Moved category collection to a helper function.

--- app/api/category_routes.py
+++ app/api/category_routes.py
@@ -21,25 +21,12 @@ def get_categories():
 
 @category_routes.route('/<int:category_id>/products/')
 def get_category_products(category_id):
-    """Returns all products associated with a category."""
+    """Returns all products associated with a category, including sub-categories."""
     try:
-        # our category structure is only 2 levels deep
-        # so in the request you will either recieve an id for a
-        # child category, in which case return items belonging to that child
-        # or a parent category, in which case collect all of its CHILDREN ids first,
-        # and return all items belonging to the children
-        categories = Category.query.all()
-        root = Category.convert_list_to_tree(categories)
-        parent_categories = dict([(child.id, child) for child in root.children])
-        if category_id in parent_categories.keys():
-            parent = parent_categories[category_id]
-            query_categories = [int(child.id) for child in parent.children]
-        else:
-            query_categories = [category_id]
-        # find products using "WHERE category_id in (x, y, z)" query
+        # if the category has children, we need to include children in the search
+        query_categories = get_category_ids_to_include_in_query(query_id)
         products = Product.query.filter(Product.category_id.in_(query_categories)).all()
         if products:
-            # normalize as id->product dict before sending
             products = dict([(p.id, p.to_dict()) for p in products])
             return products
         return {}
@@ -47,16 +34,38 @@ def get_category_products(category_id):
         message = error.__repr__()
         return {"errors": "category/products GET failed", "message": message}
 
+
 @category_routes.route('/<int:category_id>/products/page/<int:page_number>')
 def get_category_products_paginated(category_id, page_number):
     """Returns 12 products from a given category, for the given page number."""
-    print("I got here.")
-    PRODUCTS_PER_PAGE = 3
-    query_categories = [category_id];
-    products = Product.query \
-        .filter(Product.category_id.in_(query_categories)) \
-        .paginate(page_number, PRODUCTS_PER_PAGE, False).items
-    if products:
-        products = dict([(p.id, p.to_dict()) for p in products])
-        return products
-    return {}
+    try:
+        PRODUCTS_PER_PAGE = 12
+        query_categories = get_category_ids_to_include_in_query(category_id)
+        products = Product.query \
+            .filter(Product.category_id.in_(query_categories)) \
+            .paginate(page_number, PRODUCTS_PER_PAGE, False).items
+        if products:
+            products = dict([(p.id, p.to_dict()) for p in products])
+            return products
+        return {}
+    except Exception as error:
+        message = error.__repr__()
+        return {"errors": "category/products GET failed", "message": message}
+
+
+def get_category_ids_to_include_in_query(category_id):
+    """Given a category id, returns list of all children and self."""
+    # our category structure is only 2 levels deep
+    # so in the request you will either recieve an id for a
+    # child category, in which case return items belonging to that child
+    # or a parent category, in which case collect all of its CHILDREN ids first,
+    # and return all items belonging to the children and the category itself
+    # note: should have included children as a column in the db!
+    categories = Category.query.all()
+    root = Category.convert_list_to_tree(categories)
+    parent_categories = dict([(child.id, child) for child in root.children])
+    query_categories = [category_id]
+    if category_id in parent_categories.keys():
+        parent = parent_categories[category_id]
+        query_categories.extend([int(child.id) for child in parent.children])
+    return query_categories
