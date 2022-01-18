04ba7943f2f301107ce27cbb3660ef1343de4044
suchitra <suchimohan91@gmail.com>
2022-01-18 12:59:14 -0800

    fixed search on thanh's comment

--- react-app/src/components/Navigation/SearchForm.js
+++ react-app/src/components/Navigation/SearchForm.js
@@ -13,11 +13,14 @@ const [searchKeyWord,setSearchKeyWord] = useState('')
 const handleKeyPress = async (e) =>{
     if (e.charCode === 13) {
         e.preventDefault();
+        if(searchKeyWord === ""){
+            history.push(`/category/root/page/1`)
+        } else {
         let resultFromSearch = await dispatch(search(searchKeyWord));
         if (resultFromSearch) {
             history.push(`/search/${searchKeyWord}`)
         }
-    }
+    }}
 }
 
 return (
